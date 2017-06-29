e9513352d854768a0cd8641e58d4a9cabb7a9475
Unknown <fernando_gentico@yahoo.com.ar>
2017-06-29 20:58:57 -0300

    DETALLE ALBUM INTERNET (FALTA DISEÑO VISTA)

---
+++ app/src/main/java/digitalhouse/android/a0317moacns1c_03/Controller/AlbumController.java
@@ -0,0 +1,55 @@
+package digitalhouse.android.a0317moacns1c_03.Controller;
+
+import android.content.Context;
+
+
+import digitalhouse.android.a0317moacns1c_03.Model.DAO.DAOAlbumInternet;
+import digitalhouse.android.a0317moacns1c_03.Model.Pojo.Album;
+import digitalhouse.android.a0317moacns1c_03.utils.HTTPConnectionManager;
+import digitalhouse.android.a0317moacns1c_03.utils.ResultListener;
+
+/**
+ * Created by dh3 on 19/06/17.
+ */
+
+public class AlbumController {
+
+    private Context context;
+
+    public AlbumController(Context context) {
+        this.context = context;
+    }
+
+
+
+    public void obtenerAlbum(final ResultListener<Album> resultListenerFromView){
+
+
+
+        if(HTTPConnectionManager.isNetworkingOnline(context)){
+
+            //SI HAY CONEXION PIDO A INTERNET LOS DATOS Y LOS ALMACENO EN LA BASE DE DATOS.
+
+            DAOAlbumInternet daoAlbumInternet = new DAOAlbumInternet();
+            daoAlbumInternet.getAlbumFromInternet(new ResultListener<Album>() {
+
+                @Override
+                public void finish(Album album) {
+
+ /*                   //EL RESULTADO DE LA LISTA DE TRACKS SE GUARDA EN LA BASE DE DATOS
+                    DAOAlbumDataBase daoAlbumDataBase = new DAOAlbumDataBase(context);
+                    daoAlbumDataBase.addAlbum(album);
+
+                    resultListenerFromView.finish(album);*/
+                }
+            });
+        }
+        else {
+            //SI NO HAY CONEXION UTILIZO LOS DATOS ALMACENADOS EN LA BASE DE DATOS.
+/*
+            DAOAlbumDataBase daoAlbumDatabase = new DAOAlbumDataBase(context);
+            Album album = daoAlbumDatabase.getAllAlbums();
+            resultListenerFromView.finish(album);*/
+        }
+    }
+}
