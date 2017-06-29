bfdb6b3178b7116c6e6f54d8c00ada7612b7ab60
Unknown <federicochertok@gmail.com>
2017-06-29 20:59:51 -0300

    activityMain

---
+++ app/src/main/java/digitalhouse/android/a0317moacns1c_03/FragmentPrincipal.java
@@ -0,0 +1,34 @@
+package digitalhouse.android.a0317moacns1c_03;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import digitalhouse.android.a0317moacns1c_03.Model.Pojo.Artista;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentPrincipal extends Fragment {
+
+    
+
+    public FragmentPrincipal() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_principal, container, false);
+    }
+
+}
