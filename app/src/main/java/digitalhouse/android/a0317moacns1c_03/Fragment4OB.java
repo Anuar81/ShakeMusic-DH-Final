11aca919c094ba17804c8becd8a58d7ed0c3c2fd
Unknown <federicochertok@gmail.com>
2017-07-15 16:34:37 -0300

    COMMIT FRAGMENTS OB

---
+++ app/src/main/java/digitalhouse/android/a0317moacns1c_03/Fragment4OB.java
@@ -0,0 +1,29 @@
+package digitalhouse.android.a0317moacns1c_03;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment4OB extends Fragment {
+
+
+    public Fragment4OB() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment4_ob, container, false);
+    }
+
+}
