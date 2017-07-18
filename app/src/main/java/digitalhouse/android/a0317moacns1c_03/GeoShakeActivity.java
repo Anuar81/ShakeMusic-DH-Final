6102fdfd955b2abdd7b6098b4802b1a3522d48ac
Unknown <fernando_gentico@yahoo.com.ar>
2017-07-17 22:02:37 -0300

    Comienzo de Fb para geoshakes

--- app/src/main/java/digitalhouse/android/a0317moacns1c_03/GeoShakeActivity.java
+++ app/src/main/java/digitalhouse/android/a0317moacns1c_03/GeoShakeActivity.java
@@ -12,6 +12,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -32,8 +34,14 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -43,20 +51,16 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
     private GoogleApiClient mGoogleApiClient;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private Location mLastLocation;
-
-    // 1
     private LocationRequest mLocationRequest;
     private boolean mLocationUpdateState;
-    // 2
     private static final int REQUEST_CHECK_SETTINGS = 2;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_shake);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -76,22 +80,52 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     protected void onStart() {
         super.onStart();
-        // 2
+
         mGoogleApiClient.connect();
+
+        //lee todos los que grabaron en la base de datos y los dibuja
+
+/*    public void leerComplejo(View view){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference();
+
+        databaseReference.child("Geo").child("userid").setValue().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Geo> geoList = new ArrayList<Geo>();
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    geoList.add(snapshot.getValue(Geo.class));
+                }
+
+                Toast.makeText(HomeActivity.this, tweetList.get(1).getNombre(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(HomeActivity.this, "Fallo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }*/
+
+
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        // 3
-        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
+
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
         }
     }
 
 
     protected void placeMarkerOnMap(LatLng location) {
-        // 1
+
         MarkerOptions markerOptions = new MarkerOptions().position(location);
         markerOptions.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource
                 (getResources(), R.mipmap.ic_launcher_round3))); //Para cambiar el ícono (agregar icono a mipmap)
@@ -99,7 +133,7 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
         String titleStr = getAddress(location);
         markerOptions.title(titleStr);
 
-        // 2
+
         mMap.addMarker(markerOptions);
     }
 
@@ -112,66 +146,81 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
             return;
         }
 
-        mMap.setMyLocationEnabled(true); // 1
+        mMap.setMyLocationEnabled(true);
 
         LocationAvailability locationAvailability =
-                LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient); // 2
+                LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient);
         if (null != locationAvailability && locationAvailability.isLocationAvailable()) {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient); // 3
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
-            if (mLastLocation != null) {    // 4
+            if (mLastLocation != null) {
                 LatLng currentLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation
                         .getLongitude());
                 placeMarkerOnMap(currentLocation);
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 12));
+
+                //guarda en la base de datos la última Lat/Long de cada usuario (id random) con la canción
+
+/*                public void escribirComplejo(View view){
+
+                    Tweet tweet = new Tweet();
+                    tweet.setNombre("Edu");
+                    tweet.setApellido("Parra");
+                    tweet.setLastTweet("soy un pobre programador.");
+
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference databaseReference = database.getReference();
+                    databaseReference.child("twitters").push().setValue(tweet);
+                }*/
+
             }
         }
     }
 
 
-    private String getAddress( LatLng latLng ) {
-        // 1
-        Geocoder geocoder = new Geocoder( this );
+    private String getAddress(LatLng latLng) {
+
+        Geocoder geocoder = new Geocoder(this);
         String addressText = "";
         List<Address> addresses = null;
         Address address = null;
         try {
-            // 2
-            addresses = geocoder.getFromLocation( latLng.latitude, latLng.longitude, 1 );
-            // 3
+
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+
             if (null != addresses && !addresses.isEmpty()) {
                 address = addresses.get(0);
                 for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
-                    addressText += (i == 0)?address.getAddressLine(i):("\n" + address.getAddressLine(i));
+                    addressText += (i == 0) ? address.getAddressLine(i) : ("\n" + address.getAddressLine(i));
                 }
             }
-        } catch (IOException e ) {
+        } catch (IOException e) {
         }
         return addressText;
     }
 
 
     protected void startLocationUpdates() {
-        //1
+
         if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     LOCATION_PERMISSION_REQUEST_CODE);
             return;
         }
-        //2
+
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest,
                 this);
     }
 
 
-    // 1
     protected void createLocationRequest() {
         mLocationRequest = new LocationRequest();
-        // 2
+
         mLocationRequest.setInterval(10000);
-        // 3
+
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
@@ -187,19 +236,19 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
             public void onResult(@NonNull LocationSettingsResult result) {
                 final Status status = result.getStatus();
                 switch (status.getStatusCode()) {
-                    // 4
+
                     case LocationSettingsStatusCodes.SUCCESS:
                         mLocationUpdateState = true;
                         startLocationUpdates();
                         break;
-                    // 5
+
                     case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
                         try {
                             status.startResolutionForResult(GeoShakeActivity.this, REQUEST_CHECK_SETTINGS);
                         } catch (IntentSender.SendIntentException e) {
                         }
                         break;
-                    // 6
+
                     case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
                         break;
                 }
@@ -208,7 +257,6 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
     }
 
 
-    // 1
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -220,14 +268,14 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
         }
     }
 
-    // 2
+
     @Override
     protected void onPause() {
         super.onPause();
         LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
     }
 
-    // 3
+
     @Override
     public void onResume() {
         super.onResume();
@@ -237,20 +285,6 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
     }
 
 
-
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -258,9 +292,9 @@ public class GeoShakeActivity extends FragmentActivity implements OnMapReadyCall
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.setOnMarkerClickListener(this);
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker and move the camera
         LatLng dh = new LatLng(-34.6211321, -58.3816387);
-        mMap.addMarker(new MarkerOptions().position(dh).title("Aquí Shakers"));
+        mMap.addMarker(new MarkerOptions().position(dh).title("Aquí Shakers")); //marcador de prueba en Digital
         mMap.moveCamera(CameraUpdateFactory.newLatLng(dh)); //Centrar
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dh, 12));  //Zoom del marcador
     }
